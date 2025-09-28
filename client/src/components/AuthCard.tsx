1726ccdefc182da2984077efeed6ec72fe2ef4d3
phicae4f <mila.masterova@gmail.com>
2025-09-28 11:20:08 +0300

    register, login ui

--- client/src/components/AuthCard.tsx
+++ client/src/components/AuthCard.tsx
@@ -1,32 +1,50 @@
-import { PiCat } from "react-icons/pi"
-import { CustomInput } from "../ui/CustomInput"
-import { Link } from "react-router-dom"
+import { PiCat } from "react-icons/pi";
+import { CustomInput } from "../ui/CustomInput";
+import { Link } from "react-router-dom";
 
 interface AuthCard {
-    title: string,
-    btnText: string
+  title: string;
+  btnText: string;
 }
 
 export const AuthCard = (props: AuthCard) => {
-    return (
-        <div className="auth">
-                <div className="auth__wrapper">
-                    <h1 className="auth__title">{props.title}</h1>
-                    <div className="auth__card">
-                        <div className="auth__card-left">
-                            <PiCat size={100}/>
-                        </div>
-                        <div className="auth__card-right">
-                            <h2 className="auth__card-title">Добро Пожаловать!</h2>
-                            <form className="auth__card-form">
-                                <CustomInput type="text" name="login" id="login" placeholder="Логин"/>
-                                <CustomInput type="password" name="password" id="password" placeholder="Пароль"/>
-                                <button className="auth__card-btn" type="submit">{props.btnText}</button>
-                            </form>
-                            {props.title == "Авторизация" ? <Link to="/register">Еще нет аккаунта?</Link> : <Link to="/login">Уже есть аккаунт?</Link>}
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="auth">
+      <div className="auth__wrapper">
+        <h1 className="auth__title">{props.title}</h1>
+        <div className="auth__card">
+          <div className="auth__card-left">
+            <PiCat size={100} />
+          </div>
+          <div className="auth__card-right">
+            <h2 className="auth__card-title">Добро Пожаловать!</h2>
+            <form className="auth__card-form">
+              <CustomInput
+                type="text"
+                name="login"
+                id="login"
+                placeholder="Логин"
+              />
+              <CustomInput
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Пароль"
+              />
+              <button className="auth__card-btn" type="submit">
+                {props.btnText}
+              </button>
+            </form>
+            <span className="auth__card-right-link">
+              {props.title == "Авторизация" ? (
+                <Link to="/register">Еще нет аккаунта?</Link>
+              ) : (
+                <Link to="/login">Уже есть аккаунт?</Link>
+              )}
+            </span>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- client/src/ui/CustomInput.tsx
+++ client/src/ui/CustomInput.tsx
@@ -1,14 +1,25 @@
+import { FaLock, FaUser } from "react-icons/fa";
 
 interface CustomInputProps {
-    type: string,
-    name: string,
-    id: string,
-    placeholder: string
+  type: string;
+  name: string;
+  id: string;
+  placeholder: string;
 }
-
 
 export const CustomInput = (props: CustomInputProps) => {
-    return (
-        <input className="custom-input" type={props.type} name={props.name} id={props.id} placeholder={props.placeholder}/>
-    )
-}
+  return (
+    <div className="custom-input">
+      <span className="custom-input__icon">
+        {props.name == "login" ? <FaUser size={21} /> : <FaLock size={21} />}
+      </span>
+      <input
+        className="custom-input__field"
+        type={props.type}
+        name={props.name}
+        id={props.id}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
