c7a99f33ea4c2deaca2163c7422e1a45a5d49716
phicae4f <mila.masterova@gmail.com>
2025-10-20 12:55:19 +0300

    all applications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,51 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.scss'
-import { MainPage } from './pages/MainPage'
-import { Layout } from './components/Layout'
-import { LoginPage } from './pages/LoginPage'
-import { RegisterPage } from './pages/RegisterPage'
-import { ProtectedRoute } from './components/ProtectedRoute'
-import { MyApplications } from './pages/MyApplications'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import { MainPage } from "./pages/MainPage";
+import { Layout } from "./components/Layout";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { MyApplications } from "./pages/MyApplications";
+import { AllApplications } from "./pages/AllApplications";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout><MainPage/></Layout>} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <MainPage />
+            </Layout>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
 
-        <Route path='/my-applications' element={<ProtectedRoute requiredRole="user">
-          <Layout><MyApplications /></Layout>
-        </ProtectedRoute>}/>
+        <Route
+          path="/my-applications"
+          element={
+            <ProtectedRoute requiredRole="user">
+              <Layout>
+                <MyApplications />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/applications"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <Layout>
+                <AllApplications />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
