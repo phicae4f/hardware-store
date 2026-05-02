0b040152e352b25aeada861b657eefe94befebc7
phicae4f <149423171+phicae4f@users.noreply.github.com>
2026-05-02 14:14:17 +0300

    final

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,16 +1,11 @@
 import { AboutUsSection } from "../components/AboutUsSection";
-import { CarouselSection } from "../components/CarouselSection";
 import { ContactUsSection } from "../components/ContactUsSection";
 import { EquipmentSection } from "../components/EquipmentSection";
 import { HeroSection } from "../components/HeroSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ReviewsSection } from "../components/ReviewsSection";
 import { ServicesSection } from "../components/ServicesSection";
-import {
-  constructionData,
-  designData,
-  repairData,
-} from "../mocks/carouselData";
+
 
 export const MainPage = () => {
   return (
